07162e083ba9e533fcea6172cdc0ed27f8f615e8
busiXI <70853798+busiXI@users.noreply.github.com>
2022-01-04 23:46:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -44,6 +44,7 @@ sed -i '103s/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_ge
 
 #替换tiny-tp-link.mk文件第471行字符
 sed -i '471s/4mlzma/16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
+cat target/linux/ar71xx/image/tiny-tp-link.mk
 
 #删除02_network文件第142行
 sed -i '142d' target/linux/ar71xx/base-files/etc/board.d/02_network
@@ -51,18 +52,23 @@ sed -i '142d' target/linux/ar71xx/base-files/etc/board.d/02_network
 #在153行末尾新增tl-wr802n-v1)设定，新增WAN口LAN口
 #sed -i 'N;153a\\ttl-wr802n-v1)\n\t\tucidef_set_interface_wan \"eth0\"\n\t\tucidef_set_interface_lan \"eth1\"\n\t\t\;\;' target/linux/ar71xx/base-files/etc/board.d/02_network #不能用
 sed -i '153a \\ttl-wr802n-v1)\n\t\tucidef_set_interface_wan \"eth0\"\n\t\tucidef_set_interface_lan \"eth1\"\n\t\t\;\;' target/linux/ar71xx/base-files/etc/board.d/02_network
+cat target/linux/ar71xx/base-files/etc/board.d/02_network
 #在88行末尾新增，设置WAN信息，默认网口为WAN口
 sed -i '88a \\tath79_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_MII\;\n\tath79_eth1_data.duplex = DUPLEX_FULL\;\n\tath79_eth1_data.speed = SPEED_100\;\n\tath79_eth1_data.phy_mask = BIT(4)\;\n\tath79_init_mac(ath79_eth1_data.mac_addr, mac, 2)\;\n\tath79_register_eth(1)\;\n' target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr802n.c
-
+cat target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr802n.c
 #默认开启wifi
 sed -i '116s/1/0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+cat package/kernel/mac80211/files/lib/wifi/mac80211.sh
  
  #修改feeds.conf.default文件
 #sed -i '8a src-git kenzo https\:\/\/github.com\/kenzok8\/openwrt-packages\nsrc-git small https\:\/\/github.com\/kenzok8\/small' feeds.conf.default
 sed -i '$a src-git kenzo https\:\/\/github.com\/kenzok8\/small-package' feeds.conf.default
 sed -i '$a src-git small https\:\/\/github.com\/kenzok8\/small' feeds.conf.default
 #sed -i '$a src-git cool https\:\/\/github.com\/coolsnowwolf\/packages' feeds.conf.default
+cat feeds.conf.default
 
+sed -i 's/skip/b99ef18516b705b3e73b15a9d5ddc99add359299b52639fe3c81dd761591d9d9/' /package/feeds/kenzo/lua-maxminddb/Makefile
+cat /package/feeds/kenzo/lua-maxminddb/Makefile
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
