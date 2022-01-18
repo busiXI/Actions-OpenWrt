2062e7e71358133143c145633c00f1cd4a567bed
busiXI <70853798+busiXI@users.noreply.github.com>
2022-01-18 23:02:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,31 +23,28 @@ git clone -b openwrt-21.02 https://github.com/openwrt/packages
 #复制libmaxminddb到openwrt/package/libs
 cp -r ./packages/libs/libmaxminddb ./openwrt/package/libs
 #复制ruby到openwrt
-cp -r ./packages/lang/ruby ./openwrt
+#cp -r ./packages/lang/ruby ./openwrt
 #删除packages源码节省空间
-rm -rf ./packages
+#rm -rf ./packages
 
 cd openwrt
 
 #下载openclash
 #git clone https://github.com/vernesong/OpenClash.git
-# Clone 项目
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
-
+#Clone 项目
+#mkdir package/luci-app-openclash
+#cd package/luci-app-openclash
+#git init
+#git remote add -f origin https://github.com/vernesong/OpenClash.git
+#git config core.sparsecheckout true
+#echo "luci-app-openclash" >> .git/info/sparse-checkout
+#git pull --depth 1 origin master
+#git branch --set-upstream-to=origin/master master
 # 编译 po2lmo (如果有po2lmo可跳过)
-pushd luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+#pushd luci-app-openclash/tools/po2lmo
+#make && sudo make install
+#popd
 
-cd ..
-cd ..
 
 #注释掉include/target.mk第16行
 sed -i '16s/^/#/' include/target.mk
